7c4c5c7b44528ad8acf6b765c9984c2e8cf98213
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-10-23 21:58:40 +0300

    added sign in card

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import React from "react";
+import { SignInCard } from "@/features/auth/components/sign-in-card";
 
 const SignInPage: React.FC = () => {
-  return <div>Sign In Page</div>;
+  return <SignInCard />;
 };
 
 export default SignInPage;
