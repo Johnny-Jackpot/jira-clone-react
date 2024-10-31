aae33ba3d806274b1a1b5f555dede769c7cd8e09
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-10-31 22:09:45 +0200

    added user button component

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React from "react";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Dropdown,
+  DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
@@ -15,6 +16,7 @@ import { useCurrent } from "@/features/auth/api/use-current";
 
 export const UserButton: React.FC = () => {
   const { data: user, isLoading } = useCurrent();
+  const { mutate: logout } = useLogout();
 
   if (isLoading) {
     return (
@@ -32,10 +34,42 @@ export const UserButton: React.FC = () => {
   const avatarFallback = (name || email || "U").charAt(0).toUpperCase();
 
   return (
-    <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300 cursor-pointer">
-      <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
-        {avatarFallback}
-      </AvatarFallback>
-    </Avatar>
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
+          <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        side="bottom"
+        className="w-60"
+        sideOffset={10}
+      >
+        <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
+          <Avatar className="size-[52px] border border-neutral-300">
+            <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
+              {avatarFallback}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">
+              {name || "User"}
+            </p>
+            <p className="text-xs text-neutral-500">{email}</p>
+          </div>
+        </div>
+        <DottedSeparator className="mb-1" />
+        <DropdownMenuItem
+          onClick={() => logout()}
+          className="h-10 flex items-center justify-center text-amber-700 font-medium cursor-pointer"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
