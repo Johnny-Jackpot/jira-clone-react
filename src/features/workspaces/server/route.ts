6db2984c0737ba4059fe9307167cb0414f9087ab
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-12-12 00:57:08 +0200

    added workspace image upload backend feature

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,16 +1,22 @@
 import { Hono } from "hono";
-import { ID, type Databases as DatabasesType } from "node-appwrite";
+import {
+  ID,
+  type Databases as DatabasesType,
+  type Storage as StorageType,
+  Models,
+} from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
 import { createWorkspaceSchema } from "@/features/workspaces/schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { WorkspaceService } from "@/features/workspaces/server/workspace-service";
 
 const app = new Hono().post(
   "/",
   sessionMiddleware,
   zValidator("json", createWorkspaceSchema),
   async (c) => {
-    const { name } = c.req.valid("json");
+    const { name, image } = c.req.valid("json");
 
     const user = c.get("user");
     const databases: DatabasesType = c.get("databases");
@@ -25,7 +31,15 @@ const app = new Hono().post(
       },
     );
 
-    return c.json({ data: workspace });
+    let updatedWorkspace: Models.Document | undefined;
+
+    if (image instanceof File) {
+      const storage: StorageType = c.get("storage");
+      const workspaceService = new WorkspaceService(storage, databases);
+      updatedWorkspace = await workspaceService.uploadImage(image, workspace);
+    }
+
+    return c.json({ data: updatedWorkspace ? updatedWorkspace : workspace });
   },
 );
 
