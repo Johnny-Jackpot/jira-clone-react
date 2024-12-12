90ff09ba8ed6ee886cac78693597ac9901103022
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-12-13 01:35:11 +0200

    updated file validation

--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -2,8 +2,22 @@ import { z } from "zod";
 
 export const createWorkspaceSchema = z.object({
   name: z.string().trim().min(1, "Required"),
-  image: z.union([
-    z.instanceof(File),
-    z.string().transform((value) => (value === "" ? undefined : value)),
-  ]).optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.instanceof(Blob).transform((blob: Blob|File) => {
+        /*
+          Weird case on Windows 10
+          Debugger panel shows that this is a File
+          but 'instance of File' returns false
+         */
+        if (blob.constructor.name !== 'File') {
+          throw new Error('Invalid File');
+        }
+
+        return new File([blob], blob.name, {type: blob.type});
+      }),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
 });
