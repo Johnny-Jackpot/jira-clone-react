4747c41e76fcd9f7b45499706eeb2671e6a18796
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-12-27 09:21:26 +0200

    protect routes from anon users

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { redirectToLoginIfNoUser } from "@/features/auth/actions";
 
-const Page: React.FC = () => {
+const Page: React.FC = async () => {
+  await redirectToLoginIfNoUser();
+
   return <div>Workspace Id </div>;
 };
 
