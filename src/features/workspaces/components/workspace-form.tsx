c78b304f129d4b9a232e8bf200086c82eef9a15b
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-12-27 15:15:29 +0200

    added an ability to remove image

--- src/features/workspaces/components/workspace-form.tsx
+++ src/features/workspaces/components/workspace-form.tsx
@@ -160,7 +160,7 @@ export const WorkspaceForm: React.FC<CreateWorkspaceFormProps> = ({
                             size="xs"
                             className="w-fit mt-2"
                             onClick={() => {
-                              field.onChange(null);
+                              field.onChange("");
                               if (inputRef.current) {
                                 inputRef.current.value = "";
                               }
