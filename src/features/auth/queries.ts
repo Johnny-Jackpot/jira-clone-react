7473c8e33734aab78a512067a441e8fa6dd76ff2
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-12-27 16:42:01 +0200

    used session client

--- src/features/auth/queries.ts
+++ src/features/auth/queries.ts
@@ -1,23 +1,13 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { getAppWriteClient } from "@/lib/appwrite";
-import { AUTH_COOKIE } from "@/features/auth/constants";
-import { Account } from "node-appwrite";
+import { createSessionClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
 import { routes } from "@/features/routes";
 
 export const getCurrentUser = async () => {
   try {
-    const requestCookies = await cookies();
-    const sessionId = requestCookies.get(AUTH_COOKIE);
-    if (!sessionId) {
-      return null;
-    }
+    const { account } = await createSessionClient();
 
-    const account = new Account(
-      getAppWriteClient().setSession(sessionId.value),
-    );
     return await account.get();
   } catch (e) {
     return null;
--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,24 +1,13 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { getAppWriteClient } from "@/lib/appwrite";
-import { AUTH_COOKIE } from "@/features/auth/constants";
-import { Account, Databases } from "node-appwrite";
+import { createSessionClient } from "@/lib/appwrite";
 import { workspacesUtils } from "@/features/workspaces/utils";
 import { getMember } from "@/features/members/utils";
 import { MemberRole } from "@/features/members/types";
 
 export const getWorkspaces = async () => {
   try {
-    const requestCookies = await cookies();
-    const sessionId = requestCookies.get(AUTH_COOKIE);
-    if (!sessionId) {
-      return null;
-    }
-
-    const client = getAppWriteClient().setSession(sessionId.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     return await workspacesUtils.getWorkspaces(user.$id, databases);
@@ -29,15 +18,7 @@ export const getWorkspaces = async () => {
 
 export const getWorkspace = async (workspaceId: string) => {
   try {
-    const requestCookies = await cookies();
-    const sessionId = requestCookies.get(AUTH_COOKIE);
-    if (!sessionId) {
-      return null;
-    }
-
-    const client = getAppWriteClient().setSession(sessionId.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     const member = await getMember({
