7191cc44230aa05c51ea72fdccafd8d77ca08690
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-01-04 01:28:27 +0200

    added useDeleteMember

--- src/features/members/api/use-delete-member.ts
+++ src/features/members/api/use-delete-member.ts
@@ -2,7 +2,6 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { toast } from "sonner";
 import { client } from "@/lib/rpc";
-import { QUERY_KEY_WORKSPACES } from "@/features/workspaces/api/use-get-workspaces";
 
 type ResponseType = InferResponseType<
   (typeof client.api.members)[":memberId"]["$delete"],
