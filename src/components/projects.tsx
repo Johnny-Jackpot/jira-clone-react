caf38d5877075d1ce6d997e2da050353dced4702
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-01-10 09:07:05 +0200

    added projects component

---
+++ src/components/projects.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { RiAddCircleFill } from "react-icons/ri";
+
+export const Projects: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Projects</p>
+        <RiAddCircleFill
+          onClick={() => {}}
+          className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Navigation } from "@/components/navigation";
 import { WorkspaceSwitcher } from "@/components/workspace-switcher";
+import {Projects} from "@/components/projects";
 
 export const Sidebar: React.FC = () => {
   return (
@@ -15,6 +16,8 @@ export const Sidebar: React.FC = () => {
       <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
+      <DottedSeparator className="my-4" />
+      <Projects />
     </aside>
   );
 };
