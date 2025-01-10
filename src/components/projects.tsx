360aefe1e70b62dca3a046ddbccdb1a2ec24289d
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-01-10 11:20:50 +0200

    added project avatar

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,6 +8,8 @@ import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { cn } from "@/lib/utils";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { Project } from "@/features/projects/types";
 
 export const Projects: React.FC = () => {
   const pathname = usePathname();
@@ -24,7 +26,7 @@ export const Projects: React.FC = () => {
           className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
         />
       </div>
-      {data?.documents?.map((project) => {
+      {data?.documents?.map((project: Project) => {
         const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
         const isActive = pathname === href;
 
@@ -36,6 +38,7 @@ export const Projects: React.FC = () => {
                 isActive && "bg-white shadow-sm hover:opacity-100 text-primary",
               )}
             >
+              <ProjectAvatar name={project.name} image={project.imagePreview} />
               <span className="truncate">{project.name}</span>
             </div>
           </Link>
---
+++ src/features/projects/components/project-avatar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+type ProjectAvatarProps = {
+  name: string;
+  image?: string;
+  className?: string;
+  fallbackClassName?: string;
+};
+
+export const ProjectAvatar: React.FC<ProjectAvatarProps> = ({
+  name,
+  image,
+  className,
+  fallbackClassName,
+}) => {
+  if (image) {
+    return (
+      <div
+        className={cn("size-5 relative rounded-md overflow-hidden", className)}
+      >
+        <Image src={image} alt={name} fill className="object-cover" />
+      </div>
+    );
+  }
+
+  return (
+    <Avatar className={cn("size-5 rounded-md", className)}>
+      <AvatarFallback
+        className={cn(
+          "text-white bg-blue-600 font-medium text-sm uppercase rounded-md",
+          fallbackClassName,
+        )}
+      >
+        {name[0]}
+      </AvatarFallback>
+    </Avatar>
+  );
+};
