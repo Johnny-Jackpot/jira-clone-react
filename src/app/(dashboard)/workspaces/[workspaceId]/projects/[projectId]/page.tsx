d54562676fc9b0142d225440a713afcf0a04a08c
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-01-21 15:35:54 +0200

    project page

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,13 +1,42 @@
 import React from "react";
 import { redirectToLoginIfNoUser } from "@/features/auth/queries";
 import { getProject } from "@/features/projects/queries";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { Button } from "@/components/ui/button";
+import { PencilIcon } from "lucide-react";
+import Link from "next/link";
 
 const Page: React.FC = async ({ params }) => {
   await redirectToLoginIfNoUser();
   const { projectId } = await params;
   const project = await getProject(projectId);
+  if (!project) {
+    throw new Error("Project not found");
+  }
 
-  return <div>Project Id: {projectId} {JSON.stringify(project)}</div>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <ProjectAvatar
+            name={project?.name}
+            image={project?.imagePreview}
+            className="size-8"
+          />
+          <p className="text-large font-semibold">{project.name}</p>
+        </div>
+        <div>
+          <Button variant="secondary" size="sm" asChild>
+            <Link
+              href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
+            >
+              <PencilIcon className="size-4 mr-2" /> Edit project
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
