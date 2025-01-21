0eafb28d44ddd4ee5855e93cfaa147f8eef7f117
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-01-21 17:40:30 +0200

    added delete project section

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { redirectToLoginIfNoUser } from "@/features/auth/queries";
 import { getProject } from "@/features/projects/queries";
 import { ProjectForm } from "@/features/projects/components/project-form";
+import DeleteProject from "@/features/projects/components/delete-project";
 
 const ProjectSettingsPage: React.FC = async ({ params }) => {
   await redirectToLoginIfNoUser();
@@ -9,8 +10,9 @@ const ProjectSettingsPage: React.FC = async ({ params }) => {
   const project = await getProject(projectId);
 
   return (
-    <div className="w-full lg:max-w-xl">
+    <div className="w-full lg:max-w-xl flex flex-col gap-y-4">
       <ProjectForm initialValues={project} />
+      <DeleteProject project={project} />
     </div>
   );
 };
---
+++ src/features/projects/components/delete-project.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useDeleteWorkspace } from "@/features/workspaces/api/use-delete-workspace";
+import { DottedSeparator } from "@/components/dotted-separator";
+import {Project} from "@/features/projects/types";
+
+type DeleteProjectProps = {
+  project: Project;
+};
+
+const DeleteProject: React.FC<DeleteProjectProps> = ({ project }) => {
+  const router = useRouter();
+
+  const [DeleteDialog, confirmDelete] = useConfirm(
+    `Delete "${project.name}" project`,
+    `Are you sure you want to delete "${project.name}" project? This action can not be undone.`,
+    "destructive",
+  );
+
+  const { mutate, isPending } = useDeleteWorkspace();
+
+  const handleDelete = async () => {
+    const confirmed = await confirmDelete();
+    if (!confirmed) {
+      return;
+    }
+
+    mutate(
+      {
+        param: { projectId: project.$id },
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    );
+  };
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <DeleteDialog />
+      <CardContent className="p-7">
+        <div className="flex flex-col">
+          <h3 className="font-bold">Danger Zone</h3>
+          <p className="text-sm text-muted-foreground">
+            Deleting project is irreversible and will remove all associated
+            data
+          </p>
+          <DottedSeparator className="py-7" />
+          <Button
+            className="mt-6 w-fit ml-auto"
+            size="sm"
+            variant="destructive"
+            type="button"
+            onClick={handleDelete}
+            disabled={isPending}
+          >
+            Delete project
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DeleteProject;
