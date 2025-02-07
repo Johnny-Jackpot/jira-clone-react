3d817f18fc2f4783d4f02fa3e8f057782e72b989
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-02-07 18:03:42 +0200

    added task form wrapper

---
+++ src/features/tasks/components/create-task-form-wrapper.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Loader } from "lucide-react";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { Project } from "@/features/projects/types";
+import { Member } from "@/features/members/types";
+import { Card, CardContent } from "@/components/ui/card";
+
+interface CreateTaskFormWrapperProps {
+  onCancel: () => void;
+}
+
+export const CreateTaskFormWrapper: React.FC<CreateTaskFormWrapperProps> = ({
+  onCancel,
+}) => {
+  const workspaceId = useWorkspaceId();
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const projectOptions = projects?.documents.map((project: Project) => ({
+    id: project.$id,
+    name: project.name,
+    imagePreview: project.imagePreview,
+  }));
+
+  const memberOptions = members?.documents.map((member: Member) => ({
+    id: member.$id,
+    name: member.name,
+  }));
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  if (isLoading) {
+    return (
+      <Card className="w-full h-[714px] border-none shadow-none">
+        <CardContent className="flex items-center justify-center h-full">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    <div>
+      {JSON.stringify(projectOptions)}
+      {JSON.stringify(memberOptions)}
+    </div>
+  );
+};
--- src/features/tasks/components/create-task-modal.tsx
+++ src/features/tasks/components/create-task-modal.tsx
@@ -3,13 +3,14 @@
 import React from "react";
 import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
 import { ResponsiveModal } from "@/components/responsive-modal";
+import { CreateTaskFormWrapper } from "@/features/tasks/components/create-task-form-wrapper";
 
 export const CreateTaskModal: React.FC = () => {
-  const { isOpen, setIsOpen } = useCreateTaskModal();
+  const { isOpen, setIsOpen, close } = useCreateTaskModal();
 
   return (
     <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
-      <div>TODO: Task form</div>
+      <CreateTaskFormWrapper onCancel={close} />
     </ResponsiveModal>
   );
 };
