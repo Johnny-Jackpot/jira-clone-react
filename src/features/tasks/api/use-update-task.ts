708f1deb043e5d37f36abd31a4dba6843c628a8e
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-02-26 23:11:13 +0200

    task edit

--- src/features/tasks/api/use-update-task.ts
+++ src/features/tasks/api/use-update-task.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { toast } from "sonner";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import { client } from "@/lib/rpc";
 
 type ResponseType = InferResponseType<
