93adab6b5d21e534ddd65b3b5cba90e65c25e629
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-03-03 18:11:40 +0200

    added kanban card

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -7,6 +7,7 @@ import {
 } from "@hello-pangea/dnd";
 import { Task, TaskStatus } from "../types";
 import { KanbanColumnHeader } from "./kanban-column-header";
+import { KanbanCard } from "./kanban-card";
 
 const boards: TaskStatus[] = [
   TaskStatus.BACKLOG,
@@ -56,6 +57,33 @@ export const DataKanban = ({ data }: DataKanbanProps) => {
             className="flex-1 mx-2 bg-muted p-1.5 rounded-md min-w-[200px]"
           >
             <KanbanColumnHeader board={board} taskCount={tasks[board].length} />
+            <Droppable droppableId={board}>
+              {(provided) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="min-h-[200px] py-1.5"
+                >
+                  {tasks[board].map((task, index) => (
+                    <Draggable
+                      key={task.$id}
+                      draggableId={task.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <KanbanCard task={task} />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              )}
+            </Droppable>
           </div>
         ))}
       </div>
