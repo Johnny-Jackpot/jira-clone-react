8cf57e9bb815da5c7a6892bd1eae83d5897e3fa0
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-01 18:16:43 +0300

    added project analytics hook

---
+++ src/features/projects/api/use-get-project-analytics.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+
+type UseGetProjectAnalyticsProps = {
+  projectId: string;
+};
+
+export const UseGetProjectAnalytics = ({
+  projectId,
+}: UseGetProjectAnalyticsProps) => {
+  return useQuery({
+    queryKey: ["project-analytics", projectId],
+    queryFn: async () => {
+      const response = await client.api.projects[":projectId"][
+        "analytics"
+      ].$get({ param: { projectId } });
+      if (!response.ok) {
+        throw new Error("Failed to fetch project analytics");
+      }
+
+      const { data } = await response.json();
+
+      return data;
+    },
+  });
+};
